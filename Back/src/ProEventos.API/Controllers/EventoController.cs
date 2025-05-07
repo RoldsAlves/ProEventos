289a141582ae30868b98c98080b24230efa21979
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2025-05-07 09:43:31 -0300

    Curso Udemy - .NET 5 seção 3 - Criação de micrations e EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,38 +13,22 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-            new Evento() {
-                    EventoId = 1,
-                    Tema = "Angular 11 e .NET 5",
-                    Local = "Salvador",
-                    Lote = "1º Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString(),
-                    ImagemURL = "foto.png"
-                },
-            new Evento() {
-                EventoId = 2,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Salvador",
-                Lote = "2º Lote",
-                QtdPessoas = 350,
-                DataEvento = DateTime.Now.AddDays(4).ToShortDateString(),
-                ImagemURL = "foto.png"
-            }
-        };
+            _context = context;
+        }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]
