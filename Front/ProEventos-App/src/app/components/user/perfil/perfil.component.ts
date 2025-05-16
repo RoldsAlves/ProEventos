23fc4fbb239dc649c5a2c24b6f4c7d5bc569e14a
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2025-05-16 15:33:38 -0300

    Curso Udemy - .NET Web API e Angular - Seção 8: Reactive Forms e Layout

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsField } from '@app/helpers/ValidatorsField';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  get f(): any {
+    return this.form.controls;
   }
 
+  constructor(public fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorsField.MustMatch('password', 'passwordConfirm')
+    };
+
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      fucntionType: ['', Validators.required],
+      description: ['', [Validators.required, Validators.maxLength(150)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordConfirm: ['', Validators.required],
+    }, formOptions);
+  }
+
+  onSubmit(): void {
+    // Vai parar aqui se o form estiver inválido, não permitindo a confirmação.
+    if(this.form.invalid){
+      return;
+    }
+  }
+
+  public resetForm(event: any): void {
+    event.preventDefault();
+    this.form.reset();
+  }
 }
--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsField } from '@app/helpers/ValidatorsField';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get f(): any {
+    return this.form.controls;
   }
 
+  constructor(public fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorsField.MustMatch('password', 'passwordConfirm')
+    };
+
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordConfirm: ['', Validators.required],
+    }, formOptions);
+  }
 }
